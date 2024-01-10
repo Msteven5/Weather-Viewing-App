3e7a3b235d1a6d266e388bfc33fa858292490f3b
Msteven <msdavid55@gmail.com>
2024-01-10 03:00:42 -0600

    nearly finialized js functionality/changed up styling decisions in html and css

--- Assets/script.js
+++ Assets/script.js
@@ -1,75 +1,101 @@
 let userForm = $('.form-control');
 
-let date =document.getElementsByClassName('item-1');
-let icon =document.getElementsByClassName('item-2');
-let temp =document.getElementsByClassName('item-3');
-let wind =document.getElementsByClassName('item-4');
-let humidity =document.getElementsByClassName('item-5');
+let savedOnScreen = JSON.parse(localStorage.getItem("savedCities"));
 
-dateArray =Array.from(date);
-iconArray =Array.from(icon);
-tempArray =Array.from(temp);
-windArray =Array.from(wind);
-humidArray =Array.from(humidity);
+let date = document.getElementsByClassName('item-1');
+let icon = document.getElementsByClassName('item-2');
+let temp = document.getElementsByClassName('item-3');
+let wind = document.getElementsByClassName('item-4');
+let humidity = document.getElementsByClassName('item-5');
+
+dateArray = Array.from(date);
+iconArray = Array.from(icon);
+tempArray = Array.from(temp);
+windArray = Array.from(wind);
+humidArray = Array.from(humidity);
 
 function loadSaves() {
-    let savedCities = JSON.parse(localStorage.getItem("savedCities")) || [];
-    let searchedCities = JSON.parse(localStorage.getItem("searchedCities"))
-
-    for (let i = 0; i < searchedCities.length; i++) {
-        savedCities.push(searchedCities[i]);
-
-        for (let i = 0; i < savedCities.length; i++) {
-            $('.searchResults').append("<li>" + savedCities[i] + "<li>")
-        }
+    $('#searchResults').empty();
+    
+    for (let i = 0; i < savedOnScreen.length; i++) {
+        let buttonOnScreen = $("<button>").text(savedOnScreen[i]).addClass("rounded-3 w-75 m-1 bg-light p-2 text-dark bg-opacity-50 border-primary searchList");
+        $('#searchResults').append(buttonOnScreen);
     }
 }
 
-function searchCity() {
+function showLast() {
+    if (savedOnScreen) {
+    let lastSearched = savedOnScreen[savedOnScreen.length - 1]
+    searchCity(lastSearched);
+} else {
+    let lastSearched = "Austin"
+    searchCity(lastSearched);
+}
+}
 
-    let currentCity = userForm.val();
-    
+function searchCity(currentCity) {
+
+
     var url = "https://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&units=imperial&appid=99ada079775ccf12a3014f095b1d2b79";
-    
+
     fetch(url)
         .then(function (response) {
             return response.json()
         })
         .then(data => {
-            console.log(data);
-    
+
+            nameOnScreen = document.querySelector('.cityName')
+
             const dataArraysNeeded = [0, 8, 16, 24, 32, 39]
             const specificData = dataArraysNeeded.map(index => data.list[index]);
-    
-                
-                for(let i = 0; i < specificData.length; i++) {
-                    const { dt_txt, weather, main, wind} = specificData[i]
-                    dateArray[i].textContent = "Date: " + dt_txt
-                    iconArray[i].textContent = weather[0].icon
-                    tempArray[i].textContent = "Temperature: " + main.temp
-                    windArray[i].textContent = "Wind speed: " + wind.speed
-                    humidArray[i].textContent = "Humidity: " + main.humidity
-    
-                }
-    
-            })
-        }
 
 
+            for (let i = 0; i < specificData.length; i++) {
+                const { dt_txt, weather, main, wind, city } = specificData[i]
+                dateArray[i].textContent = "Date: " + dt_txt
+                iconArray[i].textContent = weather[0].icon
+                tempArray[i].textContent = "Temperature: " + main.temp
+                windArray[i].textContent = "Wind speed: " + wind.speed
+                humidArray[i].textContent = "Humidity: " + main.humidity
+                nameOnScreen.textContent = "Currently: " + data.city.name;
+
+            }
+            let savedCities = JSON.parse(localStorage.getItem("savedCities")) || [];
+            let maxLength = 5;
+                if (!savedCities.includes(currentCity)) {
+                savedCities.push(currentCity)
+            }
+                else if (savedCities.length >= maxLength) {
+                savedCities.shift();
+            }
+
+            localStorage.setItem("savedCities", JSON.stringify(savedCities))
+        })
+
+        .catch(error => {
+            if (error) {
+                let errorMessage = error;
+                $('.modal').show();
+                $('#errorMessage').text("This city has not been found: " + errorMessage);
+                $('.understand').on("click", function () {
+                    $('.modal').hide();
+                })
+            }
+        });
+}
+
+$('document').ready(showLast(), loadSaves())
 
 $('form').submit(function (event) {
     event.preventDefault();
-    searchCity();
-          }) 
-        
+    let userSearch = userForm.val();
+    
+    searchCity(userSearch)
+    .then(loadSaves());
+})
 
-//           {{           let date = data.list[1].
+$('#searchResults').on("click", '.searchList', function () {
 
-            
-
-
-//             1, 9, 17, 25, 33
-
-//         })
-//     localStorage.setItem("searchedCities", currentCity);
-// })
+    let cityButton = $(this).text();
+    searchCity(cityButton);
+})
