ba0aa09a91bb69b2fd73bffdc20aac63594e6c0c
Michael Davidson <msdavid55@gmail.com>
2024-01-09 22:29:14 -0600

    fixed search bar functionality and added content to display in html

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,16 @@
 let userForm = $('.form-control');
 
+let date =document.getElementsByClassName('item-1');
+let icon =document.getElementsByClassName('item-2');
+let temp =document.getElementsByClassName('item-3');
+let wind =document.getElementsByClassName('item-4');
+let humidity =document.getElementsByClassName('item-5');
+
+dateArray =Array.from(date);
+iconArray =Array.from(icon);
+tempArray =Array.from(temp);
+windArray =Array.from(wind);
+humidArray =Array.from(humidity);
 
 function loadSaves() {
     let savedCities = JSON.parse(localStorage.getItem("savedCities")) || [];
@@ -16,54 +27,42 @@ function loadSaves() {
 
 function searchCity() {
 
-    fetch(url)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            console.log("Temperature first day is:" + data.list[4].main.temp)
-            console.log(data.list[1].weather[0].icon)
-            // ...... 4, 12, 20, 28, 36
-
-            // Next task: Fill in the temperatures for the 5 day cards
-
-            // debugger; // frozen sandbox
-            console.log(data)
-        })
-}
-
-let date =document.getElementsByClassName('item-1');
-let icon =document.getElementsByClassName('item-2');
-let temp =document.getElementsByClassName('item-3');
-let wind =document.getElementsByClassName('item-4');
-let humidity =document.getElementsByClassName('item-5');
-
-$('form').submit(function (event) {
-    event.preventDefault();
     let currentCity = userForm.val();
-
+    
     var url = "https://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&units=imperial&appid=99ada079775ccf12a3014f095b1d2b79";
-
+    
     fetch(url)
         .then(function (response) {
             return response.json()
         })
         .then(data => {
             console.log(data);
-            // Extract specific information using destructuring assignment
-            const specificData = data.list.main[1, 9, 17, 25, 33]
-            
-            const { dt_txt, icon, temp, wind, humidity } = specificData;
-        
-            // Now you can use these variables as needed
-            console.log("Date:", dt_txt);
-            console.log("Icon:", icon);
-            console.log("Temperature:", temp);
-            console.log("Wind:", wind.speed);
-            console.log("Humidity:", humidity);
-          }) 
+    
+            const dataArraysNeeded = [0, 8, 16, 24, 32, 39]
+            const specificData = dataArraysNeeded.map(index => data.list[index]);
+    
+                
+                for(let i = 0; i < specificData.length; i++) {
+                    const { dt_txt, weather, main, wind} = specificData[i]
+                    dateArray[i].textContent = "Date: " + dt_txt
+                    iconArray[i].textContent = weather[0].icon
+                    tempArray[i].textContent = "Temperature: " + main.temp
+                    windArray[i].textContent = "Wind speed: " + wind.speed
+                    humidArray[i].textContent = "Humidity: " + main.humidity
+    
+                }
+    
+            })
         }
-)
+
+
+
+$('form').submit(function (event) {
+    event.preventDefault();
+    searchCity();
+          }) 
+        
+
 //           {{           let date = data.list[1].
 
             
